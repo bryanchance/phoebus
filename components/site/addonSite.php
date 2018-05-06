4ad772d26fae7f1f3cb29c119bb7bc61f873848f
Matt A. Tobin <email@mattatobin.com>
2018-05-06 14:41:07 -0400

    no message

--- components/site/addonSite.php
+++ components/site/addonSite.php
@@ -244,7 +244,7 @@ function funcGeneratePage($_array) {
     
     // Send html header and pass the final template to Smarty
     funcSendHeader('html');
-    $smarty->compile_id = str_replace('/', '_', $GLOBALS['strRequestPath']);
+    $smarty->compile_id = hash('sha1', $_strSiteTemplate);
     $libSmarty->display('string:' . $_strSiteTemplate);
 
     // We are done here...
