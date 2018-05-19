a935a350cc4992cd8fc4265b1d10d4e4b74a3acf
Matt A. Tobin <email@mattatobin.com>
2018-05-19 06:23:52 -0400

    no message

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ const CONFIG = array(
       'palemoon' => '{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}',
       'basilisk' => '{ec8030f7-c20a-464f-9b0e-13a3a9e97384}',
       'fossamail' => '{3550f703-e582-4d05-9a08-453d09bdfdc6}',
-      'borealis' => '{}'
+    //'borealis' => '{}'
     ),
     'legacyAppID' => array(
       'firefox' => '{ec8030f7-c20a-464f-9b0e-13a3a9e97384}',
@@ -105,8 +105,6 @@ $arrayConfig = array(
     'path' => funcHTTPGetValue('path'),
   )
 );
-
-funcError(var_export(CONFIG, true), true);
 
 // ----------------------------------------------------------------------------
 
