89e38e7aeaf203617179fc11a14c679535339ef0
Matt A. Tobin <email@mattatobin.com>
2018-05-22 02:47:45 -0400

    no message

--- index.php
+++ index.php
@@ -333,6 +333,12 @@ if ($arraySoftwareState['phpRequestURI'] == '/') {
 elseif (startsWith($arraySoftwareState['phpRequestURI'], '/special/')) {
   $arraySoftwareState['requestComponent'] = 'special';
 }
+// requestPath should NEVER be set if the component isn't SITE
+// or already handled like SPECIAL
+elseif ($arraySoftwareState['requestComponent'] != 'site' &&
+        $arraySoftwareState['requestPath']) {
+  funcSendHeader('404');
+}
 
 funcError($arraySoftwareState, 1);
 
