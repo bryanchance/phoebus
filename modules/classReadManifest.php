cd031e17207ce14789cc012a5053321b0996b923
Matt A. Tobin <email@mattatobin.com>
2018-06-01 07:14:50 -0400

    no message

--- modules/classReadManifest.php
+++ modules/classReadManifest.php
@@ -246,7 +246,43 @@ class classReadManifest {
 
   // ------------------------------------------------------------------------
 
-  public function getSearchPlugins($arraySearchPluginsDB) {
+  public function getSearchPlugins() {
+    $arraySearchPluginsDB = array(
+      'search-100' => 'google.xml',
+      'search-101' => 'youtube.xml',
+      'search-102' => 'google-play.xml',
+      'search-103' => 'ask.xml',
+      'search-104' => 'merriam-webster.xml',
+      'search-105' => 'facebook.xml',
+      'search-106' => 'abbreviations-com.xml',
+      'search-107' => 'accuweather.xml',
+      'search-108' => 'amazon-com.xml',
+      'search-109' => 'amazon-co-uk.xml',
+      'search-110' => 'baidu.xml',
+      'search-111' => 'dictionary-com.xml',
+      'search-112' => 'dogpile.xml',
+      'search-113' => 'ebay.xml',
+      'search-114' => 'european-search-engine.xml',
+      'search-115' => 'imdb.xml',
+      'search-116' => 'imgur.xml',
+      'search-117' => 'ixquick.xml',
+      'search-118' => 'openstreetmap.xml',
+      'search-120' => 'pale-moon-forum.xml',
+      'search-121' => 'pcnet.xml',
+      'search-122' => 'qwant.xml',
+      'search-123' => 'reference-com.xml',
+      'search-124' => 'searx.xml',
+      'search-125' => 'startpage.xml',
+      'search-126' => 'the-online-slang-dictionary.xml',
+      'search-127' => 'the-weather-channel.xml',
+      'search-128' => 'tumblr.xml',
+      'search-129' => 'urban-dictionary.xml',
+      'search-130' => 'webopedia.xml',
+      'search-131' => 'wiktionary.xml',
+      'search-132' => 'yandex.xml',
+      'search-133' => 'pale-moon-add-ons-google.xml'
+    );
+
     $datastorePath = ROOT_PATH . DATASTORE_RELPATH . '/searchplugins/';
     $arraySearchPlugins = array();
     
