a796d5e76be69f7928cc7ad5bd2aa48803ec3cf6
Matt A. Tobin <email@mattatobin.com>
2018-06-05 11:17:23 -0400

    no message

--- modules/classGenerateContent.php
+++ modules/classGenerateContent.php
@@ -222,7 +222,7 @@ class classGenerateContent {
     );
 
     $addonXPInstall =
-      $addonManifest['xpinstall'][$addonManifest['releaseXPI']]['version'];
+      $addonManifest['xpinstall'][$addonManifest['release']]['version'];
     $addonTargetApplication =
       $addonXPInstall['targetApplication'][$this->arraySoftwareState['targetApplicationID']];
     
