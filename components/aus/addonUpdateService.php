39c5224a99178198ea6764919c0cf30d735aa622
Matt A. Tobin <email@mattatobin.com>
2018-06-05 11:14:03 -0400

    no message

--- components/aus/addonUpdateService.php
+++ components/aus/addonUpdateService.php
@@ -5,6 +5,8 @@
 
 // == | INFO | ================================================================
 
+// This needs to be moved the hell out of this file
+
 // Automatic Update Service for Add-ons responds with a valid RDF file
 // containing update information for known add-ons or sends the request
 // off to AMO (for now) if it is unknown to us.
@@ -32,6 +34,17 @@
 // ============================================================================
 
 // == | Setup | ===============================================================
+
+// Constants
+const AMO_AUS_URL = 'https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=2';
+
+// This constant is a list of Add-on IDs that should never be checked for
+const BAD_ADDON_IDS = array(
+  '{972ce4c6-7e08-4474-a285-3208198ce6fd}', // Default Theme
+  'modern@themes.mozilla.org',              // Mozilla Modern Theme
+  '{a62ef8ec-5fdc-40c2-873c-223b8a6925cc}', // GData
+  '{e2fda1a4-762b-4020-b5ad-a41df1933103}', // Lightning
+);
 
 // Include modules
 $arrayIncludes = array('sql', 'sql-creds', 'readManifest', 'generateContent');
@@ -61,6 +74,7 @@ if (!$arraySoftwareState['requestAddonID'] || !$arraySoftwareState['requestAddon
     !$arraySoftwareState['requestAppID'] || !$arraySoftwareState['requestAppVersion'] ||
     !$arraySoftwareState['requestAddonCompatMode']) {
   if (!arraySoftwareState['debugMode']) {
+    // Send blank rdf response
     $moduleGenerateContent->addonUpdateService(null);
   }
   funcError('Missing minimum required arguments.');
@@ -69,14 +83,73 @@ if (!$arraySoftwareState['requestAddonID'] || !$arraySoftwareState['requestAddon
 // Check for Moz-XPI-Update header
 if (!$arraySoftwareState['requestMozXPIUpdate']) {
   if (!$arraySoftwareState['debugMode']) {
+    // Send blank rdf response
     $moduleGenerateContent->addonUpdateService(null);
   }
   funcError('Compatibility check failed.');
 }
 
-$moduleGenerateContent->addonUpdateService(null);
+// ----------------------------------------------------------------------------
 
-$moduleGenerateContent->test();
+// Check for "Bad" Add-on IDs
+if (in_array($arraySoftwareState['requestAddonID'], BAD_ADDON_IDS) {
+  if (!$arraySoftwareState['debugMode']) {
+    // Send blank rdf response
+    $moduleGenerateContent->addonUpdateService(null);
+  }
+  funcError('"Bad" Add-on ID Detected');
+}
+
+// ----------------------------------------------------------------------------
+
+// Handle FossaMail Special Case (Send to AMO unconditionally)
+if ($arraySoftwareState['requestAppID'] == TARGET_APPLICATION_ID['fossamail']) {
+  $strAMO = 
+    AMO_AUS_URL .
+    '&id=' . $arraySoftwareState['requestAddonID'] .
+    '&version=' . $arraySoftwareState['requestAddonVersion'] .
+    '&appID=' . TARGET_APPLICATION_ID['thunderbird'] .
+    '&appVersion=' . '38.9' .
+    '&compatMode=' . $arraySoftwareState['requestAddonCompatMode'];
+  
+  funcRedirect($strAMOLink);
+}
+
+// ----------------------------------------------------------------------------
+
+// Check for Add-on Updates
+if ($arraySoftwareState['requestAppID'] == $arraySoftwareState['targetApplicationID']) {
+  $addonManifest =
+    $moduleReadManifest->getAddonByID($arraySoftwareState['requestAddonID']);
+
+  if (!$addonManifest) {
+    // Send non-existant add-ons to AMO for Basilisk
+    if ($arraySoftwareState['currentApplication'] = 'basilisk') {
+      $strAMO = 
+        AMO_AUS_URL .
+        '&id=' . $arraySoftwareState['requestAddonID'] .
+        '&version=' . $arraySoftwareState['requestAddonVersion'] .
+        '&appID=' . TARGET_APPLICATION_ID['firefox'] .
+        '&appVersion=' . '52.9' .
+        '&compatMode=' . $arraySoftwareState['requestAddonCompatMode'];
+      
+      funcRedirect($strAMOLink);
+    }
+
+    // Add-on is non-existant send blank rdf response
+    $moduleGenerateContent->addonUpdateService(null);
+  }
+  
+  // Add-on exists so send update.rdf
+  $moduleGenerateContent->addonUpdateService($addonManifest);
+}
+else {
+  if (!$arraySoftwareState['debugMode']) {
+    // Send blank rdf response
+    $moduleGenerateContent->addonUpdateService(null);
+  }
+  funcError('Mismatched or Invalid Application ID');
+}
 
 // ============================================================================
 
