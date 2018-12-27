ad597516a91f71b7d5a1aa7162af18abb23b8d8e
Matt A. Tobin <email@mattatobin.com>
2018-12-27 08:52:56 -0500

    no message

--- components/panel/panelAdministration.php
+++ components/panel/panelAdministration.php
@@ -90,7 +90,7 @@ elseif ($arraySoftwareState['requestPanelTask'] == 'update') {
           'repository'    => funcUnifiedVariable('post', 'repository'),
           'supportURL'    => funcUnifiedVariable('post', 'supportURL'),
           'supportEmail'  => funcUnifiedVariable('post', 'supportEmail'),
-          'supportEmail'  => funcUnifiedVariable('post', 'tags'),
+          'tags'  => funcUnifiedVariable('post', 'tags'),
           'content'       => funcUnifiedVariable('post', 'content')
         );
 
