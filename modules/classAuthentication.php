a9411b1bdbc65bca889d3c0dbbcb7d6d5d603ee2
Matt A. Tobin <email@mattatobin.com>
2018-12-28 23:00:54 -0500

    no message

--- modules/classAuthentication.php
+++ modules/classAuthentication.php
@@ -45,27 +45,8 @@ class classAuthentication {
     $strPassword = funcUnifiedVariable('server', 'PHP_AUTH_PW');
 
     // Check for the existance of username and password as well as the special 'logout' user
-    if (!$strUsername || $strUsername == 'logout' || !$strPassword ) {
+    if (!$strUsername || !$strPassword) {
       $this->promptCredentials();
-    }
-
-    // This will handle a logout situation using a dirty javascript trick
-    // It will not work without javascript or on IE but then again neither will the PANEL
-    if ($aLogout) {
-      $url = 'https://logout:logout@' . $GLOBALS['arraySoftwareState']['currentDomain'] . '/panel/logout/';
-      funcSendHeader('html');
-      die(
-        '<html><head><script>' .
-        'var xmlHttp = new XMLHttpRequest();' .
-        'xmlHttp.open( "GET", "' . $url . '", false );' .
-        'xmlHttp.send( null );' .
-        'window.location = "/panel/";' .
-        '</script></head><body>' .
-        '<p>Logging out...</p>' .
-        '<p>If you are not redirected you also are not logged out. Enable Javascript or stop using IE/Edge!<br>' .
-        'Additionally, you can just close the browser or clear private data.</p>' .
-        '</body></html>'
-      );
     }
 
     // ----------------------------------------------------------------------------------------------------------------
@@ -75,8 +56,7 @@ class classAuthentication {
 
     // If nothing from SQL or the user isn't active or the password doesn't match
     // then reprompt until the user cancels
-    if (!$userManifest || !$userManifest['active'] ||
-        !password_verify($strPassword, $userManifest['password'])) {
+    if (!$userManifest || !$userManifest['active'] || !password_verify($strPassword, $userManifest['password'])) {
       $userManifest = null;
       $this->promptCredentials();
     }
@@ -110,6 +90,22 @@ class classAuthentication {
     header('HTTP/1.0 401 Unauthorized');   
     funcError('You need to enter a valid username and password.');
     exit();
+  }
+
+  /********************************************************************************************************************
+  * Uses a javascript hack to log a user out
+  ********************************************************************************************************************/
+  private function logout() {
+    //header('WWW-Authenticate: Basic realm="' . SOFTWARE_NAME . '"');
+    header('HTTP/1.0 401 Unauthorized');
+    funcSendHeader('html');
+    die('<html><head>'.
+        '<script>window.location = "/panel/";</script>' .
+        '</head><body>' .
+        '<p>Logging out...</p>' .
+        '<p>If you are not redirected you also are not logged out. Enable Javascript or stop using IE/Edge!<br>' .
+        'Additionally, you can just close the browser or clear private data.</p>' .
+        '</body></html>');
   }
 }
 
