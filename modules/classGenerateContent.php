a5ad985b28aab3714f8cd270d4820d2759a7bb01
Matt A. Tobin <email@mattatobin.com>
2019-01-08 14:22:10 -0500

    no message

--- modules/classGenerateContent.php
+++ modules/classGenerateContent.php
@@ -203,7 +203,7 @@ class classGenerateContent {
     }
 
     // The Panel should NEVER be cached
-    if ($GLOBALS['arraySoftwareState']['component'] == 'panel') {
+    if ($GLOBALS['arraySoftwareState']['requestComponent'] == 'panel') {
       header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
       header("Cache-Control: post-check=0, pre-check=0", false);
       header("Pragma: no-cache");
