4b6f3bce7ca6fa060cb12e98594f09ce9c4598cb
Matt A. Tobin <email@mattatobin.com>
2019-01-08 11:20:24 -0500

    no message

--- modules/classAccount.php
+++ modules/classAccount.php
@@ -41,8 +41,8 @@ class classAccount {
                                                       $GLOBALS['arraySoftwareState']['authentication']['level']);
 
     foreach ($allUsers as $_key => $_value) {
+      unset($allUsers[$_key]['password'];
       $allUsers[$_key]['addons'] = json_decode($_value['addons']);
-      unset($allUsers[$_key]['addons']
     }
 
     return $allUsers;
