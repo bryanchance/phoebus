1a90471cb1a5f52c4d103f1f6b840dce8f0eb774
Matt A. Tobin <email@mattatobin.com>
2019-01-09 09:46:32 -0500

    no message

--- components/panel/addonPanel.php
+++ components/panel/addonPanel.php
@@ -320,7 +320,7 @@ elseif (startsWith($arraySoftwareState['requestPath'], URI_ADMIN)){
           }
 
           // Do not allow editing of users at or above a user level unless they are you or you are level 5
-          if ($userManifest['username'] == $arraySoftwareState['authentication']['username'] ||
+          if ($userManifest['username'] != $arraySoftwareState['authentication']['username'] ||
               $userManifest['level'] >= $arraySoftwareState['authentication']['level']) {
             funcError('You attempted to alter a user account that is the same or higher rank as you but not you. You\'re in trouble!');
           }
