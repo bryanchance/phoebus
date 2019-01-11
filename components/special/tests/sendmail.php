9aa7c16232d8b0ca9be7b1a525876e7f4d951d96
Matt A. Tobin <email@mattatobin.com>
2019-01-11 09:58:35 -0500

    no message

---
+++ components/special/tests/sendmail.php
@@ -0,0 +1,17 @@
+<?php
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// == | Main | ========================================================================================================
+
+ini_set("SMTP", "vmx.palemoon.org");
+ini_set("sendmail_from", "phoebus@addons.palemoon.org");
+
+$sendmail = mail('email@mattatobin.com', 'Phoebus sendmail test case', 'Test');
+
+funcError($sendmail, 99);
+
+// ====================================================================================================================
+
+?>
